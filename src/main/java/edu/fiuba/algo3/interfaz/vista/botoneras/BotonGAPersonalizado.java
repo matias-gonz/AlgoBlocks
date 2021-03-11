3ab383d9955fdaa4e7abc55b6c8b51a25a99db1d
Lucas Gabriel <lgtorres@fi.uba.ar>
2021-03-11 16:11:28 -0300

    update colores + detalle con los elementos guardados

--- src/main/java/edu/fiuba/algo3/interfaz/vista/botoneras/BotonGAPersonalizado.java
+++ src/main/java/edu/fiuba/algo3/interfaz/vista/botoneras/BotonGAPersonalizado.java
@@ -1,5 +1,6 @@
 package edu.fiuba.algo3.interfaz.vista.botoneras;
 
+import edu.fiuba.algo3.interfaz.ContenedorBloque;
 import edu.fiuba.algo3.interfaz.vista.SectorBloquesDisponibles;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
@@ -11,6 +12,7 @@ import javafx.scene.paint.Color;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class BotonGAPersonalizado extends Button {
 
@@ -19,15 +21,28 @@ public class BotonGAPersonalizado extends Button {
 
     public BotonGAPersonalizado(TextField texto,  VBox contenedorAlgoritmo, SectorBloquesDisponibles sector) {
         super(texto.getText());
-        this.setStyle("-fx-base: rgba(25,208,214)");
+
+        Random rand = new Random();
+        int r = rand.nextInt(255);
+        int g = rand.nextInt(255);
+        int b = rand.nextInt(255);
+        this.setStyle("-fx-base: rgb(" + String.join(",", String.valueOf(r), String.valueOf(g), String.valueOf(b)) + ")");
+
         ImageView image = new ImageView("file:src/main/java/edu/fiuba/algo3/interfaz/imagenes/guardar.png");
         image.setFitHeight(20);
         image.setFitWidth(20);
         this.setGraphic(image);
         this.setPadding(new Insets(5));
         this.setBorder(new Border(new BorderStroke(Color.BLACK,
-                BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
+                BorderStrokeStyle.DASHED, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
+
         contenedorBloques.addAll(contenedorAlgoritmo.getChildren());
+
+        // con esto evitamos que los bloques del algoritmo guardado no se puedan borrar.
+        contenedorBloques.forEach( contenedor -> {
+            ((ContenedorBloque) contenedor).getChildren().get(0).setOnMouseClicked(null);
+        });
+
         this.botonera = sector;
 
         System.out.println("CREACION : " + this.contenedorBloques.size());
